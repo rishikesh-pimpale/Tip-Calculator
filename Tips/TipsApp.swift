37f2dabc42314b8ca89a5a22d369b73cdb9a4b63
Rishikesh Pimpale <36237167+rishikesh-pimpale@users.noreply.github.com>
2021-12-27 17:08:52 -0600

    Initial Commit

---
+++ Tips/TipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipsApp.swift
+//  Tips
+//
+//  Created by Rishikesh Pimpale on 12/27/21.
+//
+
+import SwiftUI
+
+@main
+struct TipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
