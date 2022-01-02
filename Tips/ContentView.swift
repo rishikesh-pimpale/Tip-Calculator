fc269249360a4aebfb4214f9480a2e27506bdf3a
Rishikesh Pimpale <rishikesh_pimpale@redmane.com>
2022-01-02 07:22:47 -0600

    tip calculator logic

--- Tips/ContentView.swift
+++ Tips/ContentView.swift
@@ -8,9 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var total = "100"        // default amount
+    @State var tipPercent = 15.0    // default tip percent
+    
     var body: some View {
-        Text("Hello, world!")
+        
+        VStack {
+            
+            // title
+            Text("Tip Calculator")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+            
+            // placeholder/input for amount
+            HStack {
+                Text("$")
+                TextField("Amount", text: $total)
+            }
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            HStack{
+                
+                // scale for tip percentage
+                Slider(value: $tipPercent,
+                       in: 5...25,
+                       step: 1.0)
+                
+                Text("\(Int(tipPercent))%")
+            }
             .padding()
+            
+            // validate input
+            if let totalNum = Double(total){
+                
+                let calculatedTip = (totalNum * tipPercent) / 100.0
+                
+                VStack{
+                    
+                    // display tip amount
+                    Text("Tip Amount: $\(calculatedTip, specifier: "%0.2f")")
+                        .padding()
+                    
+                    // display total amount
+                    Text("Total $\(totalNum + calculatedTip, specifier: "%0.2f")")
+                        .padding()
+                }
+            }
+            else{
+                Text("Please enter a numeric value")
+            }
+        }
     }
 }
 
